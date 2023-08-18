d0a59ee6c80ed176b628c8a40096bae423162cfd
Nathan Austin <nathan@chillchain.net>
2023-08-18 03:15:21 +0100

    - clean up of nav and broken things

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -9,28 +9,35 @@ class CartController extends Controller
     public function index()
     {
         $subtotal = 0;
-        $shipping = 4.99;
+        $shipping = 0;
         $total = 0;
-        $vat = 1.20;
+        $vat = 0;
         $item = [];
 
-        foreach(session('cart') as $id => $details) {
-            $product = Product::find($id);
-            $subtotal += $product['price'] * $details['quantity'];
+        if(session('cart')) {
 
-            $item[] = [
-                'id' => $product['id'],
-                'name' => $product['name'],
-                'description' => $product['description'],
-                'price' => $product['price'],
-                'photo' => $product['photo'],
-                'quantity' => $details['quantity'],
-                'subtotal' => $product['price'] * $details['quantity']
-            ];
+            $shipping = 4.99;
+            $vat = 1.20;
 
+            foreach(session('cart') as $id => $details) {
+                $product = Product::find($id);
+                $subtotal += $product['price'] * $details['quantity'];
+
+                $item[] = [
+                    'id' => $product['id'],
+                    'name' => $product['name'],
+                    'description' => $product['description'],
+                    'price' => $product['price'],
+                    'photo' => $product['photo'],
+                    'quantity' => $details['quantity'],
+                    'subtotal' => $product['price'] * $details['quantity']
+                ];
+
+            }
+
+            $total += round(($subtotal + $shipping) * $vat, 2);
         }
 
-        $total += round(($subtotal + $shipping) * $vat, 2);
 
         return view('cart', compact('item', 'subtotal', 'total', 'shipping', 'vat'));
     }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,10 +5,6 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-{{--                    <a href="{{ route('market') }}">--}}
-{{--                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />--}}
-{{--                    </a>--}}
-
                     <div class="shrink-0 flex items-center">
                         <a href="/">
                             <img src="images/kin-logo.png" class="object-cover h-10 w-15 "/>
